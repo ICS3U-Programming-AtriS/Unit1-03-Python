0d6a6e349a8e54234e915a57e60846a2f7848a9d
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-02-12 03:49:45 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: Feb 11, 2025
+# This program is a story of the computer gaining sentience
+
+
+def main():
+    # The Computer has a message for the world!
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
